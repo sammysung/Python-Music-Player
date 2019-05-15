8641c87b83127e1219a31e88dd9075d4e9c4e87e
sammysung <samwoodthirteen@gmail.com>
2019-05-14 21:48:39 -0500

    Simple Menu Threading Support
    Used threading to have a "Menu" thread for controlling playback, and a "Player" thread for VLC to play the music files. Also added:
    
    - List structure to allow next/previous function
    - Controls to add a different directory than the default I've set
    - A restart button if you want to listen to a song from the beginning
    
    Need to add command line args, then work on GUI stuff.

--- test.py
+++ test.py
@@ -1,4 +1,39 @@
-import vlc, time, os, random, threading
+import time
+check = ""
+go = 0
+
+
+def clr():
+    import subprocess, os
+    print("\n"*100)
+    cmd = 'clear' if os.name == 'posix' else 'cls'
+    subprocess.call(cmd, shell=True)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def ctrl():
+    import threading
+    rand = input("Do you want to pick a random track? [y/n] \n")
+    dir2 = "/media/sam/Cross_Store/Music/"
+    if rand.lower() == "y":
+        direc = input("Do you want to input the path to your music, or use the default? [y/n]\nDefault: " + dir2 + "\n")
+        if direc.lower() == "y":
+            dir2 = input("Input the highest level directory you want to scan from: \n")
+    one = threading.Thread(target=play, args=(rand, dir2,), name="Player")
+    one.start()
+    two = threading.Thread(target=menu, name="Menu", daemon=True)
+    two.start()
+    one.join()
+    two.join()
+
+
+# one.join()
+# two.join()
+# one.start()
+# one.join()
+# two.start()
+# two.join()
+
 
 # Use this one for just a specific folder (in this case, SMT3 Disc 1)
 
@@ -6,63 +41,112 @@ import vlc, time, os, random, threading
 
 # This dir will catch all of my music
 
-dir2 = "/media/sam/Cross_Store/Music/"
-rand = input("Do you want to pick a random track? [y/n] ")
-if rand.lower() == 'y':
-    # exclude = set(["Windows"])
+def play(rand, dir2):
+    import vlc, os, random
+    global check
+    global go
+    #rand = input("Do you want to pick a random track? [y/n] \n")
+    if rand.lower() == 'y':
+        # exclude = set(["Windows"])
 
-    fileset = []
-    for path, dirs, files in os.walk(dir2):
-        dirs[:] = [d for d in dirs if not d == "Windows"]
-        for filename in files:
-            if filename.endswith(".mp3"):
-                fileset.append(os.path.join(path, filename))
+        # Just use the fileset itself for making the files if you don't want to make a randomized list to go backward.
 
-    # This way works well enough if you don't care to be picky with your subdirectories
+        fileset = []
+        for path, dirs, files in os.walk(dir2):
+            dirs[:] = [d for d in dirs if not d == "Windows"]
+            for filename in files:
+                if filename.endswith(".mp3"):
+                    fileset.append(os.path.join(path, filename))
 
-    # files = [os.path.join(path, filename)
-    #         for path, dirs, files in os.walk(dir2):
-    #        dirs[:] = [d for d in dirs if not d == "Windows"]
-    #         for filename in files
-    #         if filename.endswith(".mp3")]
-    # file = random.choice(fileset)
-    # print("Now playing: \n" + file)
-    # p = vlc.MediaPlayer(file)
+        i = 0
+        le = len(fileset)
+        lis = []
+        while i < le:
+            lis.append(random.choice(fileset))
+            i += 1
 
-    count = 0
-    while count < len(fileset):
-        file = random.choice(fileset)
-        print("Now playing: \n" + file)
-        p = vlc.MediaPlayer(file)
-        p.play()
-        time.sleep(1)
-        while p.is_playing() == 1:
-            if p.is_playing() == 0:
-                break
-            cmd = input("Do you want to [P]ause, [N]ext Track, or [S]top? Other input will check song completion. ")
-            if cmd.lower() == "s":
-                break
-            elif cmd.lower() == 'p':
-                p.pause()
-                d = input("Press any key to continue playback, blank to end playback... ")
-                #    print(d)
-                #    print(len(d))
-                if len(d) != 0:
-                    p.play()
-                    time.sleep(1)
-                else:
+        # This way works well enough if you don't care to be picky with your subdirectories
+
+        # files = [os.path.join(path, filename)
+        #         for path, dirs, files in os.walk(dir2):
+        #        dirs[:] = [d for d in dirs if not d == "Windows"]
+        #         for filename in files
+        #         if filename.endswith(".mp3")]
+        # file = random.choice(fileset)
+        # print("Now playing: \n" + file)
+        # p = vlc.MediaPlayer(file)
+
+        count = 0
+        re = 0
+        file = ''
+        print(le)
+        while count < le:
+            if re == 0:
+                # file = random.choice(fileset)
+                file = lis[count]
+            print("\nNow playing: \n" + file)
+            #print("Index is %d" % count)
+            p = ""
+            p = vlc.MediaPlayer(file)
+            p.play()
+            time.sleep(4)
+            re = 0
+            while p.is_playing() == 1:
+                #    if p.is_playing() == 0:
+                #        break
+
+                #    cmd = input("Do you want to [P]ause, [N]ext Track, or [S]top? Other input will check song completion. ")
+                if check.lower() == "s":
+                    count = le
                     break
-            elif cmd.lower() == 'n':
-                print("Skipping Track!")
-                p.stop()
-                count += 1
-                continue
-            if p.is_playing() == 0:
-                break
-            pass
-        print("Done!")
-        p.stop()
-        count += 1
+                elif check.lower() == "h":
+                    p.pause()
+                    d = input("Press any key to continue playback, blank to end playback... \n")
+                    #print(d)
+                    #print(len(d))
+                    check = ""
+                    if len(d) != 0:
+                        p.play()
+                        time.sleep(1)
+                        go = 0
+                    else:
+                        break
+                elif check.lower() == "n":
+                    if count == le - 1:
+                        print("Already at the end. Playing first track...")
+                        count = -1
+                        break
+                    check = ""
+                    print("Skipping Track!")
+                    #p.stop()
+                    #count += 1
+                    #go = 0
+                    #continue
+                    break
+                elif check.lower() == "r":
+                    re = 1
+                    check = ""
+                    count -= 1
+                    break
+                elif check.lower() == 'p':
+                    if count == 0:
+                        print("Already at beginning. Playing last track...")
+                        count = le - 2
+                        break
+                    print("Going back one track!")
+                    check = ""
+                    count -= 2
+                    break
+            #    if p.is_playing() == 0:
+            #        break
+            clr()
+            #print("Done!")
+            p.stop()
+            count += 1
+            if go == 0:
+                print("Controls:\n[H]old, [N]ext Track, [P]revious Track, [S]top, [R]estart\nOther input is ignored.")
+            go = 0
+            time.sleep(1)
 
     # This is single file mode, and single dir mode as well
 
@@ -70,38 +154,80 @@ if rand.lower() == 'y':
     # file = dir + ranfile
     # p = vlc.MediaPlayer(file)
 
-else:
-    p = vlc.MediaPlayer("/media/sam/Cross_Store/Music/Shin Megami Tensei "
-                    "III NOCTURNE ORIGINAL SOUNDTRACK [MP3]/Disc 1/1.23 Boss Battle.mp3")
-    p.play()
-    time.sleep(1)
-# g = p.get_state()
-# print(g)
-# t = p.audio_get_track_count()
-# y = p.audio_get_track()
-# h = p.is_playing()
-# print(t)
-# print(y)
-# print(h)
+    else:
+        p = vlc.MediaPlayer("/media/sam/Cross_Store/Music/Shin Megami Tensei "
+                            "III NOCTURNE ORIGINAL SOUNDTRACK [MP3]/Disc 1/1.23 Boss Battle.mp3")
+        p.play()
+        time.sleep(3)
+        print("\nNow playing: \n/media/sam/Cross_Store/Music/Shin Megami Tensei "
+                            "III NOCTURNE ORIGINAL SOUNDTRACK [MP3]/Disc 1/1.23 Boss Battle.mp3")
+        # g = p.get_state()
+        # print(g)
+        # t = p.audio_get_track_count()
+        # y = p.audio_get_track()
+        # h = p.is_playing()
+        # print(t)
+        # print(y)
+        # print(h)
 
-    while p.is_playing() == 1:
-        if p.is_playing() == 0:
-            break
-        cmd = input("Do you want to [P]ause or [S]top? Other input will check song completion. ")
-        if cmd.lower() == "s":
-            break
-        elif cmd.lower() == 'p':
-            p.pause()
-            d = input("Press any key to continue playback, blank to end playback... ")
-    #    print(d)
-    #    print(len(d))
-            if len(d) != 0:
-                p.play()
-                time.sleep(1)
-            else:
+        while p.is_playing() == 1:
+            if p.is_playing() == 0:
                 break
-        if p.is_playing() == 0:
+            if check.lower() == "s":
+                break
+            elif check.lower() == 'h':
+                p.pause()
+                d = input("Press any key to continue playback, blank to end playback... \n")
+                check = ""
+                #    print(d)
+                #    print(len(d))
+                if len(d) != 0:
+                    p.play()
+                    time.sleep(3)
+                    go = 0
+                else:
+                    break
+            elif check.lower() == 'n':
+                break
+            if p.is_playing() == 0:
+                break
+            pass
+        print("Done!")
+        if go == 0:
+            print("Controls:\n[H]old, [N]ext Track, [P]revious Track, [S]top, [R]estart\nOther input is ignored.")
+        p.stop()
+
+
+def menu():
+    global check
+    global go
+    time.sleep(1)
+    while True:
+        if go == 0:
+            check = input("Controls:\n[H]old, [N]ext Track, [P]revious Track, "
+                          "[S]top, [R]estart\nOther input is ignored.\n")
+            #check = input("\nDo you want to [P]ause, [N]ext Track, or [S]top? "
+            #              "Other input will check song completion. \n")
+            go = 1
+        else:
+            continue
+        #print("Check is " + check)
+        time.sleep(0.1)
+        if check.lower() == "s":
+            print("Outta here!")
             break
-        pass
-    print("Done!")
-    p.stop()
+        elif check.lower() == "p":
+            continue
+        elif check.lower() == "h":
+            continue
+        elif check.lower() == "r":
+            continue
+        elif check.lower() == "n":
+            continue
+        else:
+            # print("Bad input, try again!")
+            go = 0
+
+
+if __name__ == "__main__":
+    ctrl()
